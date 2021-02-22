b8b03da592f1c0c898bcdf50fbdd3789e044a7a2
Yedil Akhmetkarimov <yedil.akhmetkarimov@aspose.com>
2021-02-22 21:03:43 +0600

    js

---
+++ js/io-variables/script.js
@@ -0,0 +1,81 @@
+// alert('External')
+
+console.log('Message in console')
+
+document.write('<h3>Hello World!</h3>')
+
+document.write('<h2>Привет</h2><hr>')
+
+document.write("<h3>'Decode' School</h3>")
+
+document.write('<h3>"Decode" School</h3><hr>')
+
+// prompt()
+
+// var/let/const
+
+// var/let/const variableName = value: 123, 'word', true, flase
+
+// var myName = prompt()
+
+var myName = 'Decode'
+
+document.write('<h3> Welcome ' + myName + '!</h3>')
+
+document.write(`<h3> Welcome ${myName}!</h3>`)
+
+
+myVar = 111
+
+document.write(`<h5>Var: ${myVar}</h5>`)
+
+var myVar = 1234;
+
+document.write(`<h5>Var: ${myVar}</h5>`)
+
+// document.write(`<h5>Let: ${myLet}</h5>`)
+
+let myLet = 87569;
+
+myLet = 7777
+
+document.write(`<h5>Let: ${myLet}</h5>`)
+
+
+const myConst = 2021
+
+// myConst = 111
+
+document.write(`<h5>Const: ${myConst}</h5> <hr>`)
+
+
+
+// +, -, *, /, %
+
+
+document.write(`<p>10 + 20 = ${10 + 20}</p>`)
+document.write(`<p>140 - 89 = ${140 - 89}</p>`)
+document.write(`<p>34 * 11 = ${34 * 11}</p>`)
+document.write(`<p>144 / 12 = ${144 / 12}</p>`)
+document.write(`<p>7 % 2 = ${7 % 2}</p>`)
+document.write(`<p>8 % 2 = ${8 % 2}</p> <hr>`)
+
+
+// let num1 = prompt('Enter your number')
+// let num1 = parseInt(prompt('Enter your number'))
+// let num1 = parseFloat(prompt('Enter your number'))
+// let num1 = Number(prompt('Enter your number'))
+// let num1 = +(prompt('Enter your number'))
+
+let num1 = 20
+
+document.write(`<p>10 + ${num1} = ${10 + num1}</p>`)
+document.write(`<p>140 - ${num1} = ${140 - num1}</p>`)
+document.write(`<p>34 * ${num1} = ${34 * num1}</p>`)
+document.write(`<p>144 / ${num1} = ${144 / num1}</p>`)
+document.write(`<p>7 % ${num1} = ${7 % num1}</p>`)
+document.write(`<p>8 % ${num1} = ${8 % num1}</p> <hr>`)
+
+
+document.write(`<p>Math.pow(3, 4) = ${Math.pow(3, 4)}</p>`)
+document.write(`<p>Math.sqrt(49) = ${Math.sqrt(49)}</p>`)
